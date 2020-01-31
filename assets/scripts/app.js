09f0169a1c8199ce3c4c579615f1015080be7857
martylanger <langer.marty@gmail.com>
2020-01-31 11:36:01 -0500

    Add update function to app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,20 +1,20 @@
-// 'use strict'
-//
-// // use require with a reference to bundle the file and use it in this file
-// // const example = require('./example')
-//
-// // use require without a reference to ensure a file is bundled
-// // require('./example')
-//
-// // require our bookEvents file to gain access to all the event listeners
-// const bookEvents = require('./books/events')
-//
-// $(() => {
-//   // mount all of our event handlers to the correct DOM elements when the page
-//   // has finished loading
+'use strict'
+
+// use require with a reference to bundle the file and use it in this file
+// const example = require('./example')
+
+// use require without a reference to ensure a file is bundled
+// require('./example')
+
+// require my gameEvents file to gain access to all the event listeners
+const gameEvents = require('./game/events')
+
+$(() => {
+  // mount all of our event handlers to the correct DOM elements when the page
+  // has finished loading
 //   $('#books-index').on('click', bookEvents.onGetBooks)
 //   $('#books-show').on('submit', bookEvents.onGetBook)
 //   $('#books-delete').on('submit', bookEvents.onDeleteBook)
-//   $('#books-update').on('submit', bookEvents.onUpdateBook)
+  $('div.box').on('click', gameEvents.onUpdateGame)
 //   $('#books-create').on('submit', bookEvents.onCreateBook)
-// })
+})
