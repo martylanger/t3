264a15ed1f80f864108b79674aa016512a16ea58
martylanger <langer.marty@gmail.com>
2020-01-31 09:40:07 -0500

    Add much
    Initial deployment of browser template.
    Build most of game engine.
    Adjust game engine to adapt to receiving a game in progress by
      determining whose turn it is based on moves made rather than a switch
      running from the game start.
    Start reorganizing JS script.

--- index.js
+++ index.js
@@ -14,3 +14,58 @@ require('./assets/scripts/app.js')
 
 // styles
 require('./assets/styles/index.scss')
+
+require('.assets/scripts/')
+
+
+
+let xo = 'X'
+// const isEmptyString = function (cell) {
+//   return cell === ""
+// }
+// const whosTurn = function (response) {
+//   response.cells.filter(isEmptyString).length % 2 === 1 ? 'X' : 'O'
+// }
+// const turnSwitch = function () {
+//   xo === 'X' ? xo = 'O' : xo = 'X'
+// }
+
+// Is there an odd number of cells empty? If yes, it's X's turn, otherwise it's O's turn
+// const whichTurn = function (response) {
+//   xo = response.cells.filter(cell => cell === "").length % 2 === 1 ? 'X' : 'O'
+// }
+
+// id
+// cells []
+// over
+// player_x
+
+// const which = function (){
+//   return 'whichCell = ' + whichCell
+// }
+// const xoIs = function (){
+//   return 'xo = ' + xo
+// }
+
+const clickCell = function (event) {
+  // console.log('clickCell called, ' + which())
+  let xo = response.cells.filter(cell => cell === "").length % 2 === 1 ? 'X' : 'O'
+  // get position in board 0-8
+const position = event.target.id
+
+  // if cell is empty
+  if (!$(event.target).text()) {
+    $(event.target).text(xo)
+    // turnSwitch()
+
+  }
+}
+
+$(() => {
+  // console.log('pre-click')
+  $('div.box').on('click', clickCell)
+  // $('div').on('click', function (){
+    // console.log('also click')
+  // })
+  // console.log('post-click')
+})
